3681abaf21f4872750a01d636f704b64a74758ec
Tess Avitabile <tess.avitabile@gmail.com>
2025-11-11 09:54:03 -0500

    get_affordable_housing

--- ethicsfinalproject.py
+++ ethicsfinalproject.py
@@ -5,3 +5,8 @@ def get_data_frame(id):
     client     = Socrata("data.cityofnewyork.us", None)
     data    = client.get(id)
     return pd.DataFrame.from_records(data)
+
+def get_affordable_housing(affordable_housing_df):
+    zip = input("Enter a NYC zipcode: ")
+    print("Addresses of affordable housing in this zip code")
+    affordable_housing_df.loc[affordable_housing_df['postcode'] == zip, ['house_number', 'street_name', 'borough', 'postcode']]
