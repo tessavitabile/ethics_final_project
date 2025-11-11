2df58387615f0858da7f351beed76c421c71fdec
Tess Avitabile <tess.avitabile@gmail.com>
2025-11-11 10:02:30 -0500

    add return

--- ethicsfinalproject.py
+++ ethicsfinalproject.py
@@ -9,4 +9,4 @@ def get_data_frame(id):
 def get_affordable_housing(affordable_housing_df):
     zip = input("Enter a NYC zipcode: ")
     print("Addresses of affordable housing in this zip code")
-    affordable_housing_df.loc[affordable_housing_df['postcode'] == zip, ['house_number', 'street_name', 'borough', 'postcode']]
+    return affordable_housing_df.loc[affordable_housing_df['postcode'] == zip, ['house_number', 'street_name', 'borough', 'postcode']]
