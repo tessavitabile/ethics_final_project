3b1962f34bdf161bb4942c3357f88dcfe608207d
Tess Avitabile <tess.avitabile@gmail.com>
2025-12-05 11:27:24 -0500

    add imports

--- ethicsfinalproject.py
+++ ethicsfinalproject.py
@@ -1,5 +1,7 @@
 import  pandas as pd
 from    sodapy import Socrata
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 def get_data_frame(id):
     client     = Socrata("data.cityofnewyork.us", None)
